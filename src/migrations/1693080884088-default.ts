412c161baa46b9a7f09ddba92e4fc804dc0897a5
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-08-26 17:43:08 -0300

    update migrations

--- src/migrations/1693080884088-default.ts
+++ src/migrations/1693080884088-default.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Default1692763050036 implements MigrationInterface {
-    name = 'Default1692763050036'
+export class Default1693080884088 implements MigrationInterface {
+    name = 'Default1693080884088'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" text NOT NULL, "email" text NOT NULL, "password" text NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
