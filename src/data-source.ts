1e7438a6cba876e7598d21f483a1545bcd205d9f
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-08-26 18:20:26 -0300

    update dev

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,12 +8,12 @@ const port = process.env.DB_PORT as number | undefined
 
 const options: DataSourceOptions & SeederOptions = {
     type: "postgres",
-    url: process.env.DB_URI,
-    // host: process.env.DB_HOST,
-    // port: port,
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME,
+    // url: process.env.DB_URI,
+    host: process.env.DB_HOST,
+    port: port,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     entities: [`${__dirname}/**/entities/*.{ts, js}`],
     migrations: [`${__dirname}/**/migrations/*.{ts, js}`],
     seeds: [MainSeeder]
